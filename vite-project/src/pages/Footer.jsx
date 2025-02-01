2d420eb08af3288b9a06cd5e5f4e0b10dc36179b
AreebaMajid-15 <areebamajid04@gmail.com>
2025-02-01 12:59:44 +0530

    movie Added

---
+++ vite-project/src/pages/Footer.jsx
@@ -0,0 +1,20 @@
+
+//   import React from "react"
+
+
+// const Footer = ()=> {
+//     return(
+//         <div class="Footer" style={{width: "100%" , }}>
+//               <h6>Made with love</h6>
+//         </div>
+//     )
+// }
+
+// export default Footer
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>Footer</div>
+  )
+}
